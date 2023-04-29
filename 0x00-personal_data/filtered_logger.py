e6a15098d81487fd571e8e32db5a31b158b07334
Adams Adjartey <melawzy421@yahoo.com>
2023-04-29 10:31:22 +0000

    Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -35,4 +35,15 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         NotImplementedError
         return filter_datum(self.fields, self.REDACTION,
-                                        super().format(record), self.SEPARATOR)
+                            super().format(record), self.SEPARATOR)
+def get_logger() -> logging.Logger:
+    """ Implementing a logger.
+    """
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
