fe35c560ae28516ea45b308c770306a746546026
Adams Adjartey <melawzy421@yahoo.com>
2023-05-05 05:01:29 +0000

    8. Logout

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+""" Auth class.
+"""
+from flask import request
+from typing import List, TypeVar
+from os import getenv
+
+class Auth:
+    """ Class to manage the API authentication.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method
+        Args:
+        path: String type.
+        excluded_paths: List type.
+        Return:
+        True if the path is not in the list of strings.
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Method to validate all requests to protect the API.
+        Args:
+        request.
+        Return:
+        None - request.
+        """
+        if request is None:
+            return None
+        if "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers["Authorization"]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method
+        Args:
+        request.
+        Return:
+        None - request.
+        """
+        return None
+    
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+        if request is None:
+            return None
+        SESSION_NAME = getenv("SESSION_NAME")
+        if SESSION_NAME is None:
+            return None
+        session_id = request.cookies.get(SESSION_NAME)
+        return session_id
