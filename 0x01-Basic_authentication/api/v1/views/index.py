9ac4e5e26613eba684da8998272de4807d378da0
Adams Adjartey <melawzy421@yahoo.com>
2023-05-12 23:09:37 +0000

    update file

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,40 +4,38 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def raise_403() -> None:
+    """ GET /api/v1/forbidden
+    Abort:
+    - Raises 403 error to test response
+    """
+    abort(403)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def raise_401() -> None:
+    """ GET /api/v1/unauthorized
+    Abort:
+    - Raises 401 error to test response
+    """
+    abort(401)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
     Return:
-      - the status of the API
+    - the status of the API
     """
     return jsonify({"status": "OK"})
-
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
-      - the number of each objects
+    - the number of each objects
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-    - raises a 401 error by using abort
-    """
-    abort(401)
-
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-    - raises a 403 error by using abort
-    """
-    abort(403)
